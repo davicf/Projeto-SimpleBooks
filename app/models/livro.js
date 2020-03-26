f86795d9199c6c9fc691e2fa7cc1a409f1cec269
Davi Carvalho <davi_carvalho1@live.com>
2020-03-25 22:24:42 -0300

    Adicionando a Classe Livro

---
+++ app/models/livro.js
@@ -0,0 +1,13 @@
+class Livro{
+    constructor(titulo, ano, paginas, resumo, capa, nota) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.paginas = paginas;
+        this.resumo = resumo;
+        this.capa = capa;
+        this.nota = nota;
+    }
+}
+
+
+module.exports = Livro;
